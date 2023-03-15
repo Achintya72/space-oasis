ddd76d08cb4bca58c12a1ce7d26bc4c35eae332d
Achintya72 <achintya.agrawal.90@gmail.com>
2023-03-14 18:45:28 -0700

    Added About Page and Required Form Links Co-authored-by: PranavK226 <PranavK226@users.noreply.github.com>

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -16,14 +16,9 @@ export default function Footer() {
                         <Link href="/training">Training</Link>
                         <Link href="/safety">Safety</Link>
                         <Link href="/sources">Sources</Link>
+                        <Link href="/about">About</Link>
                     </div>
-                    <div className={styles.about}>
-                        <p><strong>About:</strong></p>
-                        <p>To ensure a seamless, fast, and optimized user experience, this website uses Vercel&apos;s NextJS framework,
-                            a ReactJS technology as the coding technology. However, the design system and brand themes are all
-                            original and specially designed with the user&apos;s experience and accessibilty in mind.
-                        </p>
-                    </div>
+
                 </div>
             </Wrapper>
         </footer>
