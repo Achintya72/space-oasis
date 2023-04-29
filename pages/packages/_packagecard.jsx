7a849b6f52eb338c1a45ecb736af28369dc9b77d
Achintya72 <achintya.agrawal.90@gmail.com>
2023-04-29 16:15:59 -0700

    Updated Packages Card

--- pages/packages/_packagecard.jsx
+++ pages/packages/_packagecard.jsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import getClasses from "../api/_getClasses";
 import useGetVisibility from "../api/_useGetVisibility";
+import Button from "../../components/Button";
+import Icon from "../../components/Icon";
 
 
 const options = {
@@ -32,7 +34,7 @@ export default function PackageCard({ img, title, description, price, location, 
     const starsList = getStars();
 
     return (
-        <div className={getClasses(styles.card, show ? styles.show : styles.hide)} onClick={redirect} ref={ref}>
+        <div className={getClasses(styles.card, show ? styles.show : styles.hide)} ref={ref}>
             <div className={styles.cardImg}>
                 <Image
                     src={img}
@@ -42,23 +44,37 @@ export default function PackageCard({ img, title, description, price, location, 
                     alt={description}
                 />
             </div>
-            <div style={{ display: "flex", flexDirection: "column", gap: "10px", padding: "15px" }}>
-                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                    <div>
-                        <strong>{title}</strong> <br />
-                        <div style={{ color: "#959595" }}>{location}</div>
+            <div style={{ display: "flex", flex: "1", flexDirection: "column", justifyContent: "space-between", gap: "10px" }}>
+                <div>
+                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                        <div>
+                            <strong>{title}</strong> <br />
+                            <div style={{ color: "#959595", margin: "5px 0px", display: "flex", alignItems: "center", gap: "5px" }}><Icon size={16} name="location" />  {location}</div>
+                        </div>
+                        <h3 className={styles.price}>
+                            ~${price != undefined && price.toLocaleString()}*
+                        </h3>
                     </div>
-                    <h3 className={styles.price}>
-                        ~${price != undefined && price.toLocaleString()}*
-                    </h3>
-                </div>
 
-                <p>{description} <span className={styles.viewMore}>View More...</span>
-                </p>
-                <div style={{ display: "flex", gap: "1px" }}>
-                    {starsList.map((value, index) => (
-                        <Image key={`star${index}`} className={styles.star} src={value} width={20} height={20} alt="A colored star" />
-                    ))}
+                    <p>{description}
+                    </p>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <div style={{ display: "flex", gap: "1px" }}>
+                        {starsList.map((value, index) => (
+                            <Image key={`star${index}`} className={styles.star} src={value} width={20} height={20} alt="A colored star" />
+                        ))}
+                    </div>
+                    <div style={{ display: "flex", gap: "10px" }}>
+                        <Button
+                            background="dark" hasIcon corners="sharp"
+                        ><Icon size={20} name="cart" /></Button>
+                        <Button
+                            background="dark"
+                            corners="sharp"
+                            onClick={redirect}
+                        >View Details</Button>
+                    </div>
                 </div>
             </div>
         </div>
