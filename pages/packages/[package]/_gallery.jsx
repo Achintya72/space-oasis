4a6061eb8c76357077b62172e897952542df0326
Achintya72 <achintya.agrawal.90@gmail.com>
2023-05-17 23:26:16 -0700

    Fixed Compiler Errors

--- pages/packages/[package]/_gallery.jsx
+++ pages/packages/[package]/_gallery.jsx
@@ -6,15 +6,19 @@ export default function Gallery({ images }) {
     const [current, changeCurrent] = useState(0);
     return (
         <div className={styles.galleryImg}>
-            <div className={styles.controls}>
-                {current == 0 ? <div style={{ width: "30px", height: "30px" }} /> :
-                    <Icon name="leftButton" size={30} onClick={() => changeCurrent(prev => prev - 1)} />
-                }
-                {current == images.length - 1 ? <div style={{ width: "30px", height: "30px" }} /> :
-                    <Icon name="rightButton" size={30} onClick={() => changeCurrent(prev => prev + 1)} />
-                }
-            </div>
-            <Image src={images[current].src} alt={images[current.alt]} fill />
+            {images &&
+                <>
+                    <div className={styles.controls}>
+                        {current == 0 ? <div style={{ width: "30px", height: "30px" }} /> :
+                            <Icon name="leftButton" size={30} onClick={() => changeCurrent(prev => prev - 1)} />
+                        }
+                        {current == (images?.length ?? 1) - 1 ? <div style={{ width: "30px", height: "30px" }} /> :
+                            <Icon name="rightButton" size={30} onClick={() => changeCurrent(prev => prev + 1)} />
+                        }
+                    </div>
+                    <Image src={images[current].src} alt={images[current.alt]} fill />
+                </>
+            }
         </div>
     )
 }
